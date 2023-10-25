8aa50eb846f6d6193fd9ed4d93dda236e5ca4a96
wenyuouyang <hust2014owen@gmail.com>
2023-10-25 20:23:12 +0800

    add rich logger and warning

--- hydroutils/hydro_logger.py
+++ hydroutils/hydro_logger.py
@@ -1,13 +1,46 @@
-import smtplib
-import ssl
+"""
+Author: Wenyu Ouyang
+Date: 2023-10-25 20:07:14
+LastEditTime: 2023-10-25 20:20:21
+LastEditors: Wenyu Ouyang
+Description: Use rich to log: https://rich.readthedocs.io/en/latest/
+FilePath: /hydroutils/hydroutils/hydro_logger.py
+Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
+"""
+from rich.logging import RichHandler
+import logging
+from rich.console import Console
+from rich.text import Text
+
+logging.basicConfig(
+    level="DEBUG", format="%(message)s", datefmt="[%X]", handlers=[RichHandler()]
+)
+
+hydro_logger = logging.getLogger("rich")
 
 
-# -------------------------------------------------- notification tools--------------------------------------------
-def send_email(subject, text, receiver="hust2014owen@gmail.com"):
-    sender = "hydro.wyouyang@gmail.com"
-    password = "D4VEFya3UQxGR3z"
-    context = ssl.create_default_context()
-    msg = f"Subject: {subject}\n\n{text}"
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-        server.login(sender, password)
-        server.sendmail(from_addr=sender, to_addrs=receiver, msg=msg)
+class HydroWarning:
+    def __init__(self):
+        self.console = Console()
+
+    def no_directory(self, directory_name, message=None):
+        if message is None:
+            message = Text(
+                f"There is no such directory: {directory_name}", style="bold red"
+            )
+        self.console.print(message)
+
+    def file_not_found(self, file_name, message=None):
+        if message is None:
+            message = Text(
+                f"We didn't find this file: {file_name}", style="bold yellow"
+            )
+        self.console.print(message)
+
+    def operation_successful(self, operation_detail, message=None):
+        if message is None:
+            message = Text(f"Operation Success: {operation_detail}", style="bold green")
+        self.console.print(message)
+
+
+hydro_warning = HydroWarning()
