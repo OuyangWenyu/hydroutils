479d61043936cd96066bc5d409263789cab4006c
ouyangwenyu <wenyuouyang@outlook.com>
2024-03-16 15:41:07 +0800

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/OuyangWenyu/hydroutils",
-    version='0.0.8',
+    version='0.0.9',
     zip_safe=False,
     cmdclass={
         "install": PostInstallCommand,
